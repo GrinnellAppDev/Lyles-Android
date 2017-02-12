81518b7b32a3f1b10b1f100e0e713e6b00e598de
sheltah22 <fraziers@grinnell.edu>
2017-02-12 10:55:55 -0600

    Add documentation to FavoritesManager

--- app/src/main/java/grinnell/appdev/edu/lyles/preferences/FavoritesManager.java
+++ app/src/main/java/grinnell/appdev/edu/lyles/preferences/FavoritesManager.java
@@ -16,6 +16,7 @@ import grinnell.appdev.edu.lyles.MenuItem;
 
 /**
  * Created by Shelby on 2/3/2017.
+ * A class which uses SharedPreferences to keep track of each user's favorites in each category
  */
 
 public class FavoritesManager {
@@ -26,6 +27,12 @@ public class FavoritesManager {
     private SharedPreferences.Editor editor;
     private ArrayList<MenuItem> allItems;
 
+    /**
+     * Creates a new favorites manager based on an Arraylist of menuItems
+     *
+     * @param context   the context of activity using the FavoritesManager
+     * @param menuItems the list of all items the FavoritesManager will keep track of
+     */
     public FavoritesManager(Context context, ArrayList<MenuItem> menuItems) {
 
         allItems = menuItems;
@@ -42,6 +49,12 @@ public class FavoritesManager {
         }
     }
 
+    /**
+     * Changes whether the item with id id is a favorites
+     * This should be called by the click listener for any favorite button
+     *
+     * @param id the id of the item to be favorited or unfavorited
+     */
     public void toggleFavorite(String id) {
         boolean newValue = !favorites.getBoolean(id, false);
 
@@ -52,10 +65,22 @@ public class FavoritesManager {
         editor.apply();
     }
 
+    /**
+     * Determines whether an item of id id has been favorited by the user
+     *
+     * @param  id the id of the item being investigated
+     * @return    a boolean describing whether the item has been favorited
+     */
     public boolean isFavorite(String id) {
         return favorites.getBoolean(id, false);
     }
 
+    /**
+     * Retrieves an ArrayList containing all items the user has favorited
+     * This should be called by FavoritesFragment when providing a list of elements to the adapter
+     *
+     * @return     an ArrayList containing all favorited items
+     */
     public ArrayList<MenuItem> getAllFavorites() {
         ArrayList<MenuItem> returnList = new ArrayList<MenuItem>();
         for(int i = 0; i < allItems.size(); i++) {
@@ -66,11 +91,20 @@ public class FavoritesManager {
         return returnList;
     }
 
+    /**
+     * Unfavorites all items the user has favorited
+     */
     public void clearFavorites() {
         editor.clear();
         editor.apply();
     }
 
+    /**
+     * Determines text the favorite button should display (if displaying text)
+     *
+     * @param id the id of the item to being investigated
+     * @return   String of text to be displayed
+     */
     public String getButtonText(String id) {
         if(favorites.getBoolean(id, false))
             return "Unfavorite";
@@ -78,6 +112,11 @@ public class FavoritesManager {
             return "Favorite";
     }
 
+    /**
+     * Retrieve the information contained in the file corresponding to SharedPreferences
+     *
+     * @return     the contents of the SharedPreferences file as a String
+     */
     public String getFile() {
         return favorites.getAll().toString();
     }
