614b2acaf970a52ffd20b9f4c62c679b052f4bae
Ankit Pandey <pandeyan@grinnell.edu>
2017-02-26 13:45:05 -0600

    Begin implementing Schedule

---
+++ app/src/main/java/grinnell/appdev/edu/lyles/DateSeparator.java
@@ -0,0 +1,11 @@
+package grinnell.appdev.edu.lyles;
+
+/**
+ * Created by arp on 19/02/17.
+ */
+
+public class DateSeparator implements ScheduleItemInterface {
+    public boolean isSeparator() {
+        return true;
+    }
+}
